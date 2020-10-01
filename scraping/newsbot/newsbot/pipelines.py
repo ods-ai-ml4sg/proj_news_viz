c9fda12946b65cf4fc1863b6424ea2eb72ac8bc0
Restyled.io <commits@restyled.io>
2020-10-01 15:34:18 +0000

    Restyled by black

--- scraping/newsbot/newsbot/pipelines.py
+++ scraping/newsbot/newsbot/pipelines.py
@@ -9,29 +9,46 @@ import csv
 
 class NewsbotPipeline(object):
     def open_spider(self, spider):
-        self.file = open(spider.name + '.csv', 'w', encoding='utf8')
-        self.writer = csv.writer(self.file, delimiter=',')
+        self.file = open(spider.name + ".csv", "w", encoding="utf8")
+        self.writer = csv.writer(self.file, delimiter=",")
         # Write header to the resulting file
-        self._fields = ["date", "url", "edition", "topics",
-                        "subtopics", "authors", "tags", "title", "subtitle", "text"]
-        self._metrics = ["reposts_fb", "reposts_vk", "reposts_ok", "reposts_twi",
-                         "reposts_lj", "reposts_tg", "likes", "views", "comm_count"]
-        self.file.write(','.join(self._fields + self._metrics) + '\n')
+        self._fields = [
+            "date",
+            "url",
+            "edition",
+            "topics",
+            "subtopics",
+            "authors",
+            "tags",
+            "title",
+            "subtitle",
+            "text",
+        ]
+        self._metrics = [
+            "reposts_fb",
+            "reposts_vk",
+            "reposts_ok",
+            "reposts_twi",
+            "reposts_lj",
+            "reposts_tg",
+            "likes",
+            "views",
+            "comm_count",
+        ]
+        self.file.write(",".join(self._fields + self._metrics) + "\n")
 
     def close_spider(self, spider):
         self.file.close()
 
     def process_item(self, item, spider):
-        dt = datetime.datetime.strptime(
-            item["date"][0], spider.config.date_format)
+        dt = datetime.datetime.strptime(item["date"][0], spider.config.date_format)
 
         item["title"] = spider.process_title(item["title"][0])
-        item["subtitle"] = '; '.join(
-            spider.process_title(item.get("subtitle", '-')))
-        item["topics"] = ', '.join(item.get("topics", '-'))
-        item["subtopics"] = ', '.join(item.get("subtopics", "-"))
-        item["authors"] = ', '.join(item.get("authors", "-"))
-        item["tags"] = ', '.join(item.get("tags", '-'))
+        item["subtitle"] = "; ".join(spider.process_title(item.get("subtitle", "-")))
+        item["topics"] = ", ".join(item.get("topics", "-"))
+        item["subtopics"] = ", ".join(item.get("subtopics", "-"))
+        item["authors"] = ", ".join(item.get("authors", "-"))
+        item["tags"] = ", ".join(item.get("tags", "-"))
 
         item["edition"] = item["edition"][0]
         item["url"] = item["url"][0]
@@ -42,32 +59,36 @@ class NewsbotPipeline(object):
 
         # Filtering out too late items
         if dt.date() >= spider.until_date and dt.date() <= spider.start_date:
-            line = (item["date"], item["url"], item["edition"], item["topics"],
-                    item["subtopics"],
-                    item["authors"],
-                    item['tags'],
-                    item["title"],
-                    item["subtitle"],
-                    item["text"],
-                    item["reposts_fb"],
-                    item["reposts_vk"],
-                    item["reposts_ok"],
-                    item["reposts_twi"],
-                    item["reposts_lj"],
-                    item["reposts_tg"],
-                    item["likes"],
-                    item["views"],
-                    item["comm_count"]
-                    )
+            line = (
+                item["date"],
+                item["url"],
+                item["edition"],
+                item["topics"],
+                item["subtopics"],
+                item["authors"],
+                item["tags"],
+                item["title"],
+                item["subtitle"],
+                item["text"],
+                item["reposts_fb"],
+                item["reposts_vk"],
+                item["reposts_ok"],
+                item["reposts_twi"],
+                item["reposts_lj"],
+                item["reposts_tg"],
+                item["likes"],
+                item["views"],
+                item["comm_count"],
+            )
             self.writer.writerow(line)
 
         return item
 
     def _process_metrics(self, spider, item, metrics):
         for m in metrics:
-            if spider.config.__getattribute__(m + '_path') == "_":
+            if spider.config.__getattribute__(m + "_path") == "_":
                 item[m] = "-"
             else:
-                item[m] = spider.process_metric(item.get(m, ['0']))
+                item[m] = spider.process_metric(item.get(m, ["0"]))
 
         return item
--- scraping/newsbot/newsbot/spiders/tass.py
+++ scraping/newsbot/newsbot/spiders/tass.py
@@ -11,64 +11,90 @@ class RussiaTassSpider(NewsSpider):
         title_path='//h1[contains(@class, "news-header__title") or contains(@class, "explainer__title") or contains(@class, "article__title")]//text()',
         subtitle_path='//div[contains(@class, "news-header__lead")]//text()',
         date_path='//dateformat[@mode="abs"]/@time',
-        date_format='%Y-%m-%dT%H%M%S%z',
+        date_format="%Y-%m-%dT%H%M%S%z",
         text_path='//div[contains(@class, "text-block")]/p//text()',
-        topics_path='//title/text()',
+        topics_path="//title/text()",
         tags_path='//a[contains(@class, "tags__item")]//text()',
-        subtopics_path='_',
-        authors_path='_',
-        reposts_fb_path='_',
-        reposts_vk_path='_',
-        reposts_ok_path='_',
-        reposts_twi_path='_',
-        reposts_lj_path='_',
-        reposts_tg_path='_',
-        likes_path='_',
-        views_path='_',
-        comm_count_path='_'
+        subtopics_path="_",
+        authors_path="_",
+        reposts_fb_path="_",
+        reposts_vk_path="_",
+        reposts_ok_path="_",
+        reposts_twi_path="_",
+        reposts_lj_path="_",
+        reposts_tg_path="_",
+        likes_path="_",
+        views_path="_",
+        comm_count_path="_",
     )
 
     def parse(self, response):
         # Parse main sitemap
         body = response.body
-        links = Selector(text=body).xpath('//loc/text()').getall()
-        last_modif_dts = Selector(text=body).xpath('//lastmod/text()').getall()
+        links = Selector(text=body).xpath("//loc/text()").getall()
+        last_modif_dts = Selector(text=body).xpath("//lastmod/text()").getall()
         print(last_modif_dts[0], last_modif_dts[-1])
-        if self.start_date >= datetime.strptime(max(last_modif_dts).replace(':', ''), '%Y-%m-%dT%H%M%S%z').date() >= self.until_date:
+        if (
+            self.start_date
+            >= datetime.strptime(
+                max(last_modif_dts).replace(":", ""), "%Y-%m-%dT%H%M%S%z"
+            ).date()
+            >= self.until_date
+        ):
             for link, last_modif_dt in zip(links, last_modif_dts):
                 # Convert last_modif_dt to datetime
                 last_modif_dt = datetime.strptime(
-                    last_modif_dt.replace(':', ''), '%Y-%m-%dT%H%M%S%z')
+                    last_modif_dt.replace(":", ""), "%Y-%m-%dT%H%M%S%z"
+                )
 
-                if last_modif_dt.date() >= self.until_date and last_modif_dt.date() <= self.start_date:
+                if (
+                    last_modif_dt.date() >= self.until_date
+                    and last_modif_dt.date() <= self.start_date
+                ):
                     yield Request(url=link, callback=self.parse_sub_sitemap, priority=1)
 
     def parse_sub_sitemap(self, response):
         # Parse sub sitemaps
         body = response.body
-        links = Selector(text=body).xpath('//loc/text()').getall()
-        last_modif_dts = Selector(text=body).xpath('//lastmod/text()').getall()
-        if self.start_date >= datetime.strptime(max(last_modif_dts).replace(':', ''), '%Y-%m-%dT%H%M%S%z').date() >= self.until_date:
+        links = Selector(text=body).xpath("//loc/text()").getall()
+        last_modif_dts = Selector(text=body).xpath("//lastmod/text()").getall()
+        if (
+            self.start_date
+            >= datetime.strptime(
+                max(last_modif_dts).replace(":", ""), "%Y-%m-%dT%H%M%S%z"
+            ).date()
+            >= self.until_date
+        ):
             print(max(last_modif_dts))
             print(len(links))
             for link, last_modif_dt in zip(links, last_modif_dts):
                 # Convert last_modif_dt to datetime
                 last_modif_dt = datetime.strptime(
-                    last_modif_dt.replace(':', ''), '%Y-%m-%dT%H%M%S%z')
+                    last_modif_dt.replace(":", ""), "%Y-%m-%dT%H%M%S%z"
+                )
                 if self.start_date >= last_modif_dt.date() >= self.until_date:
-                    yield Request(url=link, callback=self.parse_document, priority=100, meta={'date': last_modif_dt.strftime('%Y-%m-%dT%H%M%S%z')})
+                    yield Request(
+                        url=link,
+                        callback=self.parse_document,
+                        priority=100,
+                        meta={"date": last_modif_dt.strftime("%Y-%m-%dT%H%M%S%z")},
+                    )
 
     def parse_document(self, response):
-        print('here')
+        print("here")
         for item in super().parse_document(response):
-            print('one more')
-            item['date'] = [response.meta.get('date')]
-            if 'text' in item:
-                if re.search('/.+/. ([\d\D]+)', item['text'][0]):
-                    item['text'][0] = re.search(
-                        '/.+/. ([\d\D]+)', item['text'][0]).group(1)
-            if 'topics' in item:
-                item['topics'] = [re.search(r'-([\s\w]+)-', topic).group(1).strip()
-                                  for topic in item['topics'] if re.search(r'-([\s\w]+)-', topic)]
+            print("one more")
+            item["date"] = [response.meta.get("date")]
+            if "text" in item:
+                if re.search("/.+/. ([\d\D]+)", item["text"][0]):
+                    item["text"][0] = re.search(
+                        "/.+/. ([\d\D]+)", item["text"][0]
+                    ).group(1)
+            if "topics" in item:
+                item["topics"] = [
+                    re.search(r"-([\s\w]+)-", topic).group(1).strip()
+                    for topic in item["topics"]
+                    if re.search(r"-([\s\w]+)-", topic)
+                ]
 
             yield item
